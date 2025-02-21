3b3aeb8c305e7c26531c6d19a33e936de3d6ae6d
Pouya <jeopard13@yahoo.com>
2025-02-21 19:31:13 +0400

    chore: AccessControl for minting and burning mechanism

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
 /*
  * @title Rebase Token for Deposit Token by Nethermind
@@ -10,15 +11,27 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @notice Only Admin can decrease the interest rate
  */
 
-contract RebaseToken is ERC20 {
+contract RebaseToken is ERC20, AccessControl {
     event RebaseToken__InterestRateUpdated(uint256 newInterestRate);
     error RebaseToken__InterestRateCanOnlyDecrease();
 
     uint256 private s_interestRate = 5e10;
 
-    constructor() ERC20("RebaseToken", "RBT") {}
+    bytes32 private ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 private MINT_BURN_ACCESS = keccak256("MINT_BURN_ACCESS");
 
-    function setInterestRate(uint256 newInterestRate) public {
+    constructor() ERC20("RebaseToken", "RBT") {
+        _grantRole(MINT_BURN_ACCESS, msg.sender);
+        _grantRole(ADMIN_ROLE, msg.sender);
+    }
+
+    /*
+     * @notic manupulate the Interest Rate of the Rebase Token
+     * @params newInterestRate is the new interest rate
+     * @dev Admin can only decrease the interest rate
+     */
+
+    function setInterestRate(uint256 newInterestRate) external {
         if (newInterestRate >= s_interestRate) {
             revert RebaseToken__InterestRateCanOnlyDecrease();
         }
